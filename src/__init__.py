05e4b1f28c7896dd12f1ad564c2bd6fb9d8c1ee8
Morgan C. Nicholson <nich.morgan@outlook.com>
2023-02-04 18:10:55 -0300

    comentários

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
+#
+#! Importação necessária para carregar as implementações de interface
 from .web_ipsa import WebIpsaCalendarScrapper
 
+#! Torna inacessível as importações acima de forma direta
 __all__ = []
